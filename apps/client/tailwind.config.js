4522887b7dd6ebede89fb323d800ac2958c776cb
pglibrary80 <pglibrary80@gmail.com>
2024-05-10 17:33:26 +0900

    first commit

--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -1,8 +1,10 @@
 const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
 const { join } = require('path');
+const TailwindConfig = require('../../libs/web/ui/src/tailwind.config');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  ...TailwindConfig,
   content: [
     join(
       __dirname,
@@ -10,8 +12,4 @@ module.exports = {
     ),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
 };
---
+++ libs/web/ui-util/README.md
@@ -0,0 +1,7 @@
+# web-ui-util
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build web-ui-util` to build the library.
